b60bb1382ec0648504d9bcbec4b7ad5bf67a5988
Amin Mir <aminmir326@gmail.com>
2022-03-31 22:37:52 +0300

    Improve roundrobin.go

--- lb/roundrobin.go
+++ lb/roundrobin.go
@@ -7,15 +7,15 @@ import (
 
 type RoundRobin[T any] struct {
 	// We don't expect to have a huge number of backends to load balance
-	// Thus, it's safe to convert to int.
-	cnt      int64
+	// Thus, uint32 should be sufficient.
+	cnt      uint32
 	backends []T
 }
 
 var _ LoadBalancer[string] = &RoundRobin[string]{}
 
 func NewRoundRobin[T any](backends []T) (*RoundRobin[T], error) {
-	if len(backends) < 1 {
+	if len(backends) == 0 {
 		return nil, ErrNotEnoughBackends
 	}
 	rr := &RoundRobin[T]{
@@ -25,7 +25,14 @@ func NewRoundRobin[T any](backends []T) (*RoundRobin[T], error) {
 }
 
 func (rr *RoundRobin[T]) Next(ctx context.Context) (T, error) {
-	cnt := atomic.AddInt64(&rr.cnt, 1)
+	select {
+	case <-ctx.Done():
+		var zero T
+		return zero, ctx.Err()
+	default:
+	}
+
+	cnt := atomic.AddUint32(&rr.cnt, 1)
 	idx := int(cnt-1) % len(rr.backends)
 	return rr.backends[idx], nil
 }
